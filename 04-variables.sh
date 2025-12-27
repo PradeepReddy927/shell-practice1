2efc4bc32b9d5bedfee3aeea5defe3d105492dc7
pradeepreddy25 <prem.pradeepreddy19@gmail.com>
2025-12-27 14:54:24 +0530

    practice-shell

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,4 +5,4 @@ PERSON2=$2
 echo "$PERSON1:: Welcome $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, finally not on zoom webniar"
 echo "$PERSON1:: So, do we have deal"
-echo "$PERSON1:: Yes, if you make Ukraine joins Russia"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
